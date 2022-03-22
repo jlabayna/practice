c8708805546b9407772d4ec1e58953c993cf56eb
jlabayna <james@jameslabayna.com>
2022-03-22 10:41:21 -0400

    Reorganize

--- hackerrank/functional_programming/sum_of_odd_elements.ml
+++ hackerrank/functional_programming/sum_of_odd_elements.ml
@@ -1,5 +1,9 @@
 (* Enter your code here. Read input from STDIN. Print output to STDOUT *)
 
+let rec read_in () =
+    try let i = read_int () in i :: read_in ()
+    with End_of_file -> []
+
 (* "Required" Method Signature:
 Number Of Parameters: 1
 Parameters: [list]
@@ -9,10 +13,6 @@ let rec odd_sum ls =
     match ls with
     | hd::tl -> if hd mod 2 != 0 then hd + odd_sum tl else odd_sum tl
     | [] -> 0
-
-let rec read_in () =
-    try let i = read_int () in i :: read_in ()
-    with End_of_file -> []
 
 let () =
     print_int (odd_sum (read_in ()))
